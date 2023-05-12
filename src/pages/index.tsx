d78f6af542bc332f17e35b443757fade69a42906
Vidminas Mikucionis <5411598+Vidminas@users.noreply.github.com>
2023-05-13 00:27:45 +0100

    Add links to prototype sites

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -98,42 +98,44 @@ const links = [
       "Moodle.",
     color: "#E95800",
   },
-  // {
-  //   text: "How to Guides",
-  //   url: "https://www.gatsbyjs.com/docs/how-to/",
-  //   description:
-  //     "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-  //   color: "#1099A8",
-  // },
-  // {
-  //   text: "Reference Guides",
-  //   url: "https://www.gatsbyjs.com/docs/reference/",
-  //   description:
-  //     "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-  //   color: "#BC027F",
-  // },
-  // {
-  //   text: "Conceptual Guides",
-  //   url: "https://www.gatsbyjs.com/docs/conceptual/",
-  //   description:
-  //     "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-  //   color: "#0D96F2",
-  // },
-  // {
-  //   text: "Plugin Library",
-  //   url: "https://www.gatsbyjs.com/plugins",
-  //   description:
-  //     "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-  //   color: "#8EB814",
-  // },
-  // {
-  //   text: "Build and Host",
-  //   url: "https://www.gatsbyjs.com/cloud",
-  //   badge: true,
-  //   description:
-  //     "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-  //   color: "#663399",
-  // },
+  {
+    text: "Drupal",
+    url: "drupal/",
+    description:
+      "Drupal.",
+    color: "#1099A8",
+  },
+  {
+    text: "Wordpress",
+    url: "wp/",
+    description:
+      "Wordpress + LifterLMS.",
+    color: "#BC027F",
+  },
+  {
+    text: "Open edX",
+    url: "openedx/",
+    badge: true,
+    description:
+      "Open edX.",
+    color: "#0D96F2",
+  },
+  {
+    text: "Canvas",
+    url: "canvas/",
+    badge: true,
+    description:
+      "Instructure Canvas.",
+    color: "#8EB814",
+  },
+  {
+    text: "MediaWiki",
+    url: "mediawiki/",
+    badge: true,
+    description:
+      "MediaWiki.",
+    color: "#663399",
+  },
 ]
 
 const IndexPage: React.FC<PageProps> = () => {
@@ -169,8 +171,8 @@ const IndexPage: React.FC<PageProps> = () => {
                 {link.text}
               </a>
               {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
+                <span style={badgeStyle} aria-label="Not Ready Yet Badge">
+                  Not ready yet!
                 </span>
               )}
               <p style={descriptionStyle}>{link.description}</p>
